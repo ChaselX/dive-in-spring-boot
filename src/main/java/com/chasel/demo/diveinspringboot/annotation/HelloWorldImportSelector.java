d5cbc36d79f6691f4cdada6f4e9fec3ac424c8d1
ChaselX <973960091@qq.com>
2019-04-15 19:26:54 +0800

    feat: 自定义@Enable基于接口驱动实现

---
+++ src/main/java/com/chasel/demo/diveinspringboot/annotation/HelloWorldImportSelector.java
@@ -0,0 +1,19 @@
+package com.chasel.demo.diveinspringboot.annotation;
+
+import com.chasel.demo.diveinspringboot.configuration.HelloWorldConfiguration;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * HelloWorld {@link ImportSelector} 实现
+ *
+ * @author XieLongzhen
+ * @date 2019/4/15 19:06
+ */
+public class HelloWorldImportSelector implements ImportSelector {
+
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        return new String[]{HelloWorldConfiguration.class.getName()};
+    }
+}
